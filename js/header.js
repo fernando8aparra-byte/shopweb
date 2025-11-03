7816062fe34c56f29e98c61a8fcb766aaae634f5
fernando8aparra-byte <fernando8aparra@gmail.com>
2025-11-02 20:27:11 -0700

    Update header.js

--- js/header.js
+++ js/header.js
@@ -1,1 +1,28 @@
+function actualizarHeaderGlobal() {
+  const loginBtn = document.getElementById("loginBtn");
+  const usuario = JSON.parse(localStorage.getItem("usuario"));
 
+  if (usuario) {
+    if (loginBtn) loginBtn.style.display = "none";
+
+    if (!document.getElementById("welcomeUser")) {
+      const header = document.querySelector("header");
+      header.insertAdjacentHTML("beforeend", `
+        <span id="welcomeUser" style="font-weight:600; color:#7c4dff; margin-left:10px;">
+          Bienvenido, ${usuario.nombre} 👋
+        </span>
+      `);
+    }
+  }
+}
+
+function cerrarSesion() {
+  localStorage.removeItem("usuario");
+  location.reload();
+}
+
+function abrirCarrito() {
+  window.location.href = "index.html#carrito";
+}
+
+document.addEventListener("DOMContentLoaded", actualizarHeaderGlobal);
